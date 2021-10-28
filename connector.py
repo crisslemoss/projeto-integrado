ad5e4000f144df9a6290100df17a9bae5cc7720b
cristiane.lemos <cristianelemos@gmail.com>
2021-10-27 23:59:17 -0300

    Atualiza conector
    - cria tabela no banco se nao existir
    - cria nova coluna com nome da proposicao
    - nome da proposicao extraida do nome do arquivo

--- connector.py
+++ connector.py
@@ -10,17 +10,32 @@ try:
                                          database='votacao_camara_deputados',
                                          user='root',
                                          password='medsys')
+
+    create = "CREATE TABLE `votos` (`Proposicao` varchar(255) DEFAULT NULL," +\
+        "`NumeroVotacao` varchar(255) DEFAULT NULL," +\
+        "`Parlamentar` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL," + \
+        "`UF` varchar(100) DEFAULT NULL," + \
+        "`VotoParlamentar` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL," + \
+        "`Partido` varchar(50) DEFAULT NULL," + \
+        "`VotoPartido` varchar(50) DEFAULT NULL" + \
+        ") ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci"
+
+    cursor = connection.cursor()
+    cursor.execute(create)
+    connection.commit()
+    cursor.close()
+
     for file_name in files:
         full_path = path + file_name
 
         cursor = connection.cursor()
         for record in DBF(full_path):
 
-            values = "'" + record["NUMVOT"] + "','" + record["NOME_PAR"] + \
+            values = "'" + file_name.split('_C')[0] + "','" + record["NUMVOT"] + "','" + record["NOME_PAR"] + \
                 "','" + record["ESTADO"] + "','" + record["VOTO"] + \
                 "','" + record["PARTIDO"] + "'"
 
-            mySql_insert_query = """INSERT INTO votos (Proposicao, Parlamentar, UF, Voto, Partido) 
+            mySql_insert_query = """INSERT INTO votos (Proposicao, NumeroVotacao, Parlamentar, UF, VotoParlamentar, Partido) 
                            VALUES (""" + values + """) """
             print(mySql_insert_query)
 
